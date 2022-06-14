7b3ac05e48bc841ff8846faba56346ff3ee5ac32
paulleerentveld <paul@veldnet.com.au>
2022-06-14 18:46:59 +1000

    lab done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
 def oxford_comma(array)
+    
+    if array.count == 1
+        return array.join
+    elsif array.count == 2 
+        return array.join(" and ")
+    elsif array.count >= 3
+        output = ""
+        output << array[0..-2].join(", ") + ", and " + array[-1].to_s
+        #output << array[-1].join(", and ")
+        return output
+
+    end
+
 
 end
+
+
+
+
+
